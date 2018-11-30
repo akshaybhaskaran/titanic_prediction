61b19c87f965aebb05906b55607d1c1290726a9b
Akshay Bhaskaran <bhaskaran.a@husky.neu.edu>
2018-11-30 00:47:23 -0600

    few more code

---
+++ program.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+
+titanic_data = pd.read_csv("/Users/akshaybhaskaran/PycharmProjects/ML/TitanicPrediction/Data/train.csv")
+
+input_features = ['PassengerId', 'Pclass', 'Name', 'Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+training_X = titanic_data[input_features]
+
+print(training_X)
+
+
+
+
+
